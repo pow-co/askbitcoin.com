1eabafbd7055e4f499476dc2cce53bce646dfb95
Guillaume De Martino <guillaumedemartino@gmail.com>
2022-11-26 17:38:46 +0100

    fix typo

--- components/BoostButton.jsx
+++ components/BoostButton.jsx
@@ -10,7 +10,7 @@ import { useRelay } from '../context/RelayContext'
 const SuccessSnackbar = (props) => {
   return (<div
     className="mx-2 sm:mx-auto max-w-sm  flex flex-row items-center justify-between bg-green-200 p-3 text-sm leading-none font-medium rounded-xl whitespace-no-wrap">
-    <div class="inline-flex items-center text-green-500">
+    <div className="inline-flex items-center text-green-500">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd"
           d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
