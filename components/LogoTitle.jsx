b052276b4189d7d0a1ca6609966b4c56159abfcf
Zyler <508282+zylerdj@users.noreply.github.com>
2023-01-01 19:38:17 +0100

    update icons path

--- components/LogoTitle.jsx
+++ components/LogoTitle.jsx
@@ -4,7 +4,7 @@ import { useTheme } from "next-themes"
 const LogoTitle = () => {
   const { theme} = useTheme()
   return <>
-  {theme === "light" ? <img className='w-[180px]' src="https://askbitcoin.ai/assets/images/askbitcoin_logo_without_margins_or_background.png" /> : <img className='w-[180px]' src="https://askbitcoin.ai/assets/images/askbitcoin_logo_inverted_without_margins_or_background.png"/>}</>
+  {theme === "light" ? <img className='w-[180px]' src="/icons/askbitcoin_logo_without_margins_or_background.png" /> : <img className='w-[180px]' src="/icons/askbitcoin_logo_inverted_without_margins_or_background.png"/>}</>
 }
 
 export default LogoTitle
