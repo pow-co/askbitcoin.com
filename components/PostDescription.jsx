d2c211a2bae8728b6f596d95c0c365252ceef93a
Zyler <508282+zylerdj@users.noreply.github.com>
2023-01-08 16:56:33 +0100

    Detect links in questions and answers

--- components/PostDescription.jsx
+++ components/PostDescription.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 
+import Linkify from "linkify-react";
+
 const PostDescription = ({ bContent }) => {
     const [ description, setDescription ] = useState(bContent)
   return (
-    <div className='mt-1 text-gray-900 dark:text-white text-base leading-6 whitespace-pre-line break-words'>{description}</div>
+    <div className='mt-1 text-gray-900 dark:text-white text-base leading-6 whitespace-pre-line break-words'><Linkify options={{target: '_blank' , className: 'linkify-hover'}}>{description}</Linkify></div>
   )
 }
 
