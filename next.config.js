52b88265f8b22b42aba985398c316ede219d682b
Guillaume De Martino <guillaumedemartino@gmail.com>
2023-01-09 15:56:08 +0100

    whitelist pow.co

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://one.relayx.io; style-src 'self' 'unsafe-inline'; img-src 'self' https://avatar.relayx.com https://a.relayx.com https://berry2.relayx.com data:; font-src 'self'; connect-src 'self' https://askbitcoin.ai https://askbitcoin.com https://api.whatsonchain.com https://staging-backend.relayx.com; frame-src 'self' https://one.relayx.io; object-src 'self'; media-src 'self';"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://one.relayx.io; style-src 'self' 'unsafe-inline'; img-src 'self' https://avatar.relayx.com https://a.relayx.com https://berry2.relayx.com data:; font-src 'self'; connect-src 'self' https://askbitcoin.ai https://askbitcoin.com https://api.whatsonchain.com https://staging-backend.relayx.com https://pow.co; frame-src 'self' https://one.relayx.io; object-src 'self'; media-src 'self';"
           },
         ]
       }
